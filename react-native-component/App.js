ebd49433d15922e6dde2444da0344b5c3796ab34
Seoyoung Lee <wow9176@naver.com>
2021-03-09 05:59:13 +0900

    custom component

--- react-native-component/App.js
+++ react-native-component/App.js
@@ -1,29 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import App from './src/App';
 
-export default function App() {
-  const name = 'Seoyoung'
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-        My name is {name === 'Seoyoung' ? 'Seoyoung Lee' : 'React Native'}
-      </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 30,
-  },
-});
-
+export default App;
