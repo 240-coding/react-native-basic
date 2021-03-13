d9d9b101a3b6606c34c655764e3d5bc8ae67661e
Seoyoung Lee <wow9176@naver.com>
2021-03-14 02:57:40 +0900

    themeprovider

--- react-native-style/src/components/Button.js
+++ react-native-style/src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components/native';
 
 const ButtonContainer = styled.TouchableOpacity`
-    background-color: ${props => props.title === 'Hanbit' ? '#3498bd' : '#9b59b6'};
+    background-color: ${props => props.title === 'Hanbit' ? props.theme.blue : props.theme.purple};
     border-radius: 15px;
     padding: 15px 40px;
     margin: 10px 0px;
@@ -12,7 +12,7 @@ const ButtonContainer = styled.TouchableOpacity`
 const Title = styled.Text`
     font-size: 20px;
     font-weight: 600;
-    color: #fff;
+    color: ${props => props.theme.text};
 `;
 
 const Button = props => {
