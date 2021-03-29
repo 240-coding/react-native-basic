8e6025177d94e9ba7745185ab02817ad12e51a97
Seoyoung Lee <wow9176@naver.com>
2021-03-30 03:23:27 +0900

    leftright

--- react-native-navigation/src/navigations/Stack.js
+++ react-native-navigation/src/navigations/Stack.js
@@ -3,6 +3,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../screens/Home';
 import List from '../screens/List';
 import Item from '../screens/Item';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Platform } from 'react-native';
 
 const Stack = createStackNavigator();
 
@@ -20,13 +22,36 @@ const StackNavigation = () => {
                 },
                 headerTitleStyle: { color: '#ffffff', fontSize: 24 },
                 headerTitleAlign: 'center',
+                headerTitle: ({ style }) => (
+                    <MaterialCommunityIcons name="react" style={style} />
+                ),
             }}
         >
             <Stack.Screen name="Home" component={Home} />
             <Stack.Screen
                 name="List"
                 component={List}
-                options={{ headerTitle: 'List Screen' }}
+                options={{
+                    headerTitle: 'List Screen',
+                    headerBackTitleVisible: true,
+                    headerBackTitle: 'Prev',
+                    headerTitleStyle: { fontSize: 24 },
+                    headerTintColor: '#e74c3c',
+                    headerBackImage: ({ tintColor }) => {
+                        const style = {
+                            marginRight: 5,
+                            marginLeft: Platform.OS === 'ios' ? 11 : 0,
+                        };
+                        return (
+                            <MaterialCommunityIcons
+                                name="keyboard-backspace"
+                                size={30}
+                                color={tintColor}
+                                style={style}
+                            />
+                        );
+                    },
+                }}
             />
             <Stack.Screen name="Detail" component={Item} />
         </Stack.Navigator>
