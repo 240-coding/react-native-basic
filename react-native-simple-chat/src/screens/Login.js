45e8b6e0cee14a8a3a5731a116771f1ec2c3c13a
Seoyoung Lee <wow9176@naver.com>
2021-04-04 20:28:13 +0900

    input

--- react-native-simple-chat/src/screens/Login.js
+++ react-native-simple-chat/src/screens/Login.js
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import styled from 'styled-components/native';
-import { Text, Button } from 'react-native';
+import { Button } from 'react-native';
+import { Image, Input } from '../components';
+import { images } from '../utils/images';
 
 const Container = styled.View`
     flex: 1;
     justify-content: center;
     align-items: center;
     background-color: ${({ theme }) => theme.background};
+    padding: 20px;
 `;
 
 const Login = ({ navigation }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const passwordRef = useRef();
+
     return (
         <Container>
-            <Text style={{ fontSize: 30 }}>Login Screen</Text>
+            <Image url={images.logo} imageStyle={{ borderRadius: 8 }}/>
+            <Input
+                label="Email"
+                value={email}
+                onChangeText={text => setEmail(text)}
+                onSubmitEditing={() => passwordRef.current.focus()}
+                placeholder="Email"
+                returnKeyType="next"
+            />            
+            <Input
+                ref={passwordRef}
+                label="Password"
+                value={password}
+                onChangeText={text => setPassword(text)}
+                onSubmitEditing={() => {}}
+                placeholder="Password"
+                returnKeyType="done"
+                isPassword
+            />
             <Button title="Signup" onPress={() => navigation.navigate('Signup')} />
         </Container>
     );
---
+++ react-native-simple-chat/src/utils/images.js
@@ -0,0 +1,5 @@
+const prefix = 'https://firebasestorage.googleapis.com/v0/b/react-native-simple-chat-83092.appspot.com/o';
+
+export const images = {
+    logo: `${prefix}/dog3.jpg?alt=media`,
+};
