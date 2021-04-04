1603719776eef4055190bc53233a18436241a9b3
Seoyoung Lee <wow9176@naver.com>
2021-04-04 20:53:17 +0900

    common

--- react-native-simple-chat/src/screens/Login.js
+++ react-native-simple-chat/src/screens/Login.js
@@ -1,8 +1,10 @@
 import React, { useState, useRef } from 'react';
 import styled from 'styled-components/native';
-import { Button } from 'react-native';
+import { TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { Image, Input } from '../components';
 import { images } from '../utils/images';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { validateEmail, removeWhitespace } from '../utils/common';
 
 const Container = styled.View`
     flex: 1;
@@ -12,34 +14,60 @@ const Container = styled.View`
     padding: 20px;
 `;
 
+const ErrorText = styled.Text`
+    align-items: flex-start;
+    width: 100%;
+    height: 20px;
+    margin-bottom: 10px;
+    line-height: 20px;
+    color: ${({ theme }) => theme.errorText};
+`;
+
 const Login = ({ navigation }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const passwordRef = useRef();
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const _handleEmailChange = email => {
+        const changedEmail = removeWhitespace(email);
+        setEmail(changedEmail);
+        setErrorMessage(
+            validateEmail(changedEmail) ? '' : 'Please verify your email.'
+        );
+    };
+    const _handlePasswordChange = password => {
+        setPassword(removeWhitespace(password));
+    };
 
     return (
-        <Container>
-            <Image url={images.logo} imageStyle={{ borderRadius: 8 }}/>
-            <Input
-                label="Email"
-                value={email}
-                onChangeText={text => setEmail(text)}
-                onSubmitEditing={() => passwordRef.current.focus()}
-                placeholder="Email"
-                returnKeyType="next"
-            />            
-            <Input
-                ref={passwordRef}
-                label="Password"
-                value={password}
-                onChangeText={text => setPassword(text)}
-                onSubmitEditing={() => {}}
-                placeholder="Password"
-                returnKeyType="done"
-                isPassword
-            />
-            <Button title="Signup" onPress={() => navigation.navigate('Signup')} />
-        </Container>
+        <KeyboardAwareScrollView
+            contentContainerStyle={{ flex: 1 }}
+            extraScrollHeight={20}
+        >
+            <Container>
+                <Image url={images.logo} imageStyle={{ borderRadius: 8 }}/>
+                <Input
+                    label="Email"
+                    value={email}
+                    onChangeText={_handleEmailChange}
+                    onSubmitEditing={() => passwordRef.current.focus()}
+                    placeholder="Email"
+                    returnKeyType="next"
+                />            
+                <Input
+                    ref={passwordRef}
+                    label="Password"
+                    value={password}
+                    onChangeText={_handlePasswordChange}
+                    onSubmitEditing={() => {}}
+                    placeholder="Password"
+                    returnKeyType="done"
+                    isPassword
+                />
+                <ErrorText>{errorMessage}</ErrorText>
+            </Container>
+        </KeyboardAwareScrollView>
     );
 };
 
